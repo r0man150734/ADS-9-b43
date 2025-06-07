51323f44b06edda105a2c3dc3bf5007e71f71455
r0man150734 <124446971+r0man150734@users.noreply.github.com>
2025-06-07 22:24:15 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -2,4 +2,22 @@
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
+#include <vector>
+#include <memory>
+
+class Node {
+public:
+    char value;
+    std::vector<std::shared_ptr<Node>> children;
+
+    Node(char val) : value(val) {}
+};
+
+class PMTree {
+public:
+    std::shared_ptr<Node> root;
+
+    PMTree(const std::vector<char>& elements);
+};
+
 #endif  // INCLUDE_TREE_H_
